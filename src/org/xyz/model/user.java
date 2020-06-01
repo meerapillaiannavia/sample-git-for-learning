41b878cbc67b197403f2129e85dc24aea25416f7
meerapillaiannavia <meerapillaiannavia@gmail.com>
2020-06-01 12:49:04 -0400

    updated toString for age as well. (on top of name)

--- src/org/xyz/model/user.java
+++ src/org/xyz/model/user.java
@@ -12,13 +12,7 @@ public class user {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	@Override
-	public String toString() {
-		return "user [name=" + name + "]";
-	}
-			
-
+	
 	public int getAge() {
 		return age;
 	}
@@ -26,5 +20,10 @@ public class user {
 	public void setAge(int age) {
 		this.age = age;
 	}
-			
+
+	@Override
+	public String toString() {
+		return "user [name=" + name + ", age=" + age + "]";
+	}
+				
 }
